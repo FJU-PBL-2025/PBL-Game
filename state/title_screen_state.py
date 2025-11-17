9a1a9282d42fc474bca05f845748fb4d1e833643
NightFeather <leolee50910@gmail.com>
2025-11-17 12:35:58 +0800

    Impl Main Menu Selection

--- state/title_screen_state.py
+++ state/title_screen_state.py
@@ -1,17 +1,42 @@
 import pygame
 
 from state.game_world_state import GameWorldState
+from state.settings_state import SettingsState
 from state.state import State
 
 
 class TitleScreenState(State):
   def __init__(self, game):
     State.__init__(self, game)
+    
+    self.OPTIONS = ["Play", "Settings", "Quit"]
+    self.index = 0
 
   def update(self, delta_time: float, actions: dict):
     if actions.get(pygame.K_RETURN):
-      new_state = GameWorldState(self.game)
-      new_state.enter_state()
+      match self.index:
+        case 0:
+          new_state = GameWorldState(self.game)
+          new_state.enter_state()
+        case 1:
+          new_state = SettingsState(self.game)
+          new_state.enter_state()
+        case 2:
+          self.game.playing = False
+          self.game.running = False
+    
+    if actions.get(pygame.K_w) or actions.get(pygame.K_UP):
+      if self.index == 0:
+        self.index = len(self.OPTIONS) - 1
+      else:
+        self.index -= 1
+    
+    if actions.get(pygame.K_s) or actions.get(pygame.K_DOWN):
+      if self.index == len(self.OPTIONS) - 1:
+        self.index = 0
+      else:
+        self.index += 1
+
     self.game.reset_keys()
 
   def render(self, surface: pygame.Surface):
@@ -21,5 +46,13 @@ class TitleScreenState(State):
       "PBL Game",
       (0, 0, 0),
       self.game.GAME_W / 2,
-      self.game.GAME_H / 2
+      self.game.GAME_H / 4
     )
+    for i, v in enumerate(self.OPTIONS):
+      self.game.draw_text(
+        surface,
+        "> " + v if self.index == i else v,
+        (0, 0, 0),
+        self.game.GAME_W / 2,
+        self.game.GAME_H / 2 + i * 40
+      )
